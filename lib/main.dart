197f48e56169206541c2c426d338e78525ab81f6
karinash20 <cuteblackcb@gmail.com>
2026-04-24 17:39:48 +0500

    two commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'game.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -12,9 +14,29 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Title("A", HitType.hit),
         ),
       ),
     );
+  }
+}
+
+class Title extends StatelessWidget{
+  const Title(this.letter, this.hitType, {super.key});
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context){
+    return Container(width:60, height:60, decoration: BoxDecoration(
+      border: Border.all(color: Colors.grey.shade300),
+      color: switch (hitType){
+        HitType.hit => Colors.green,
+        HitType.miss => Colors.grey,
+        HitType.partial => Colors.yellow,
+        _=> Colors.white
+      }),
+      child: Center (child: Text(letter))
+      );
   }
 }
